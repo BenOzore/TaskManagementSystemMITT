ab25a3d700f031b055b03fad52be07f988072911
Yuxin Li <yuxinli915@outlook.com>
2020-11-09 15:11:42 -0600

    UpdatedMigrationsForDatabase

--- TaskManagementSystemMITT/Migrations/202011092110549_AddedPriority.cs
+++ TaskManagementSystemMITT/Migrations/202011092110549_AddedPriority.cs
@@ -3,15 +3,17 @@ namespace TaskManagementSystemMITT.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class addedValuesToPriorityProperty : DbMigration
+    public partial class AddedPriority : DbMigration
     {
         public override void Up()
         {
             AddColumn("dbo.Projects", "Priority", c => c.Int(nullable: false));
+            AlterColumn("dbo.Projects", "Name", c => c.String(nullable: false));
         }
         
         public override void Down()
         {
+            AlterColumn("dbo.Projects", "Name", c => c.String());
             DropColumn("dbo.Projects", "Priority");
         }
     }
